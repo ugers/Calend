aada494e416120d27aa62869fd77563a43dbdc4d
ugers <yugers@gmail.com>
2020-08-11 12:46:58 +0300

    update description, fix max number graphik, update for bip emu

--- build_res.bat
+++ build_res.bat
@@ -1,2 +1,2 @@
 @echo off
-ResPack.exe -a MNVolkov_BipOS_0.5.2_Apps_Mili_chaohu.res.latin ./stock/MNVolkov_BipOS_0.5.2_Apps_Mili_chaohu.res.latin Graphik_work.elf
+ResPack.exe -a MNVolkov_BipOS_0.5.3_Apps_Mili_chaohu.res.latin ./stock/MNVolkov_BipOS_0.5.3_Apps_Mili_chaohu.res.latin Graphik_work.elf
